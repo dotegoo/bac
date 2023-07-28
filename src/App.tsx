caeb0931558e36b786434039e2182a0105163892
dotegoo <119845829+dotegoo@users.noreply.github.com>
2023-07-28 16:50:52 +0300

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,39 +9,43 @@ import Chimie from "./chimie";
 function App() {
   return (
     <>
-      <header>
-        <h1 className="text-center">Bac</h1>
-        <nav className="navbar">
-          <div className="container">
-            <a className="navbar-brand" href="#Romana">
-              <img src="./romana.png" width="65" height="65" />
-            </a>
-            <a className="navbar-brand" href="#Matematica">
-              <img src="./matematica.png" width="65" height="65" />
-            </a>
-            <a className="navbar-brand" href="#Informatica">
-              <img src="./informatica.png" width="65" height="65" />
-            </a>
-            <a className="navbar-brand" href="#Biologie">
-              <img src="./biologie.png" width="65" height="65" />
-            </a>
-            <a className="navbar-brand" href="#Fizica">
-              <img src="./fizica.png" width="65" height="65" />
-            </a>
-            <a className="navbar-brand" href="#Chimie">
-              <img src="./chimie.png" width="65" height="65" />
-            </a>
-          </div>
-        </nav>
-      </header>
+      <div id="primaParte">
+        <header>
+          <h1 className="text-center">Bac</h1>
+          <nav className="navbar">
+            <div className="container">
+              <a className="navbar-brand" href="#Romana">
+                <img src="./romana.png" width="65" height="65" />
+              </a>
+              <a className="navbar-brand" href="#Matematica">
+                <img src="./matematica.png" width="65" height="65" />
+              </a>
+              <a className="navbar-brand" href="#Informatica">
+                <img src="./informatica.png" width="65" height="65" />
+              </a>
+              <a className="navbar-brand" href="#Biologie">
+                <img src="./biologie.png" width="65" height="65" />
+              </a>
+              <a className="navbar-brand" href="#Fizica">
+                <img src="./fizica.png" width="65" height="65" />
+              </a>
+              <a className="navbar-brand" href="#Chimie">
+                <img src="./chimie.png" width="65" height="65" />
+              </a>
+            </div>
+          </nav>
+        </header>
 
-      <Despre />
-      <Romana />
-      <Matematica />
-      <Informatica />
-      <Biologie />
-      <Fizica />
-      <Chimie />
+        <Despre />
+        <Romana />
+        <Matematica />
+      </div>
+      <div id="aDouaParte">
+        <Informatica />
+        <Biologie />
+        <Fizica />
+        <Chimie />
+      </div>
     </>
   );
 }
