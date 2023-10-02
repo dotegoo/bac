baa71f30ba0c1e2d5f88f6c952b46f70a713db48
dotegoo <119845829+dotegoo@users.noreply.github.com>
2023-10-02 21:07:29 +0300

    23.10.2

--- src/components/romana.tsx
+++ src/components/romana.tsx
@@ -4,6 +4,13 @@ const Romana = () => {
     <>
       <div id='Romana'>
         <h1 className='text-center'>Romana</h1>
+        <nav>
+          <ul>
+            <li>Item 1</li>
+            <li>item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </nav>
       </div>
     </>
   );
--- src/home.tsx
+++ src/home.tsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import "../App.css";
-import Despre from "./despre";
+import "./App.css";
+import Despre from "./components/despre";
 const Home: React.FC = () => {
   return (
     <div id='app'>
