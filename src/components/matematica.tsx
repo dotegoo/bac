9da8b58879acd86c59b0c5b9e09e00c5a5709a76
dotegoo <teodordobromir@gmail.com>
2024-01-08 04:00:41 +0200

    24.01.08

--- src/components/matematica.tsx
+++ src/components/matematica.tsx
@@ -2,24 +2,50 @@ import "../App.css";
 const Matematica = () => {
   return (
     <>
-       <div id='Matematica'>
+      <div id='Matematica'>
         <h2 className='text-center'>Matematica</h2>
-        <h1 id="Var-Bar-h1"className='text-center'>Variante</h1>
-        <div id="Var-Bar">
-        <div id="2020"><h1>2020</h1></div>
-        <div id="2021"><h1>2021</h1></div>
-        <div id="2022"><h1>2022</h1></div>
-        <div id="2023"><h1>2023</h1></div>
-        <div id="2024"><h1>2024</h1></div>
-      </div>
-      <h1 id="Var-Bar-h1"className='text-center'>Bareme</h1>
-      <div id="Var-Bar">
-        <div id="2020"><h1>2020</h1></div>
-        <div id="2021"><h1>2021</h1></div>
-        <div id="2022"><h1>2022</h1></div>
-        <div id="2023"><h1>2023</h1></div>
-        <div id="2024"><h1>2024</h1></div>
-      </div>
+        <h1 id='Var-Bar-h1' className='text-center'>
+          Variante
+        </h1>
+        <div id='Var-Bar'>
+          <a href='/biologie.png' download>
+            <div id='2020'>
+              <h1>2020</h1>
+            </div>
+          </a>
+          <div id='2021'>
+            <h1>2021</h1>
+          </div>
+          <div id='2022'>
+            <h1>2022</h1>
+          </div>
+          <div id='2023'>
+            <h1>2023</h1>
+          </div>
+          <div id='2024'>
+            <h1>2024</h1>
+          </div>
+        </div>
+        <h1 id='Var-Bar-h1' className='text-center'>
+          Bareme
+        </h1>
+        <div id='Var-Bar'>
+          <div id='2020'>
+            <h1>2020</h1>
+          </div>
+          <div id='2021'>
+            <h1>2021</h1>
+          </div>
+          <div id='2022'>
+            <h1>2022</h1>
+          </div>
+          <div id='2023'>
+            <h1>2023</h1>
+          </div>
+          <div id='2024'>
+            <h1>2024</h1>
+          </div>
+        </div>
       </div>
     </>
   );
