b6687f4991a949fa47228f971db6785dbe96fed5
Teodor Dobromir <teodor@Teodors-MacBook-Air.local>
2024-11-13 20:25:04 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <h1>Bacu'</h1>
+      <nav>
+        <ul>
+          <a href=""><li>Romana</li></a>
+          <a href=""><li>Matematica</li></a>
+          <a href=""><li>Biologie</li></a>
+          <a href=""><li>Chimie</li></a>
+          <a href=""><li>Fizica</li></a>
+          <a href=""><li>Informatica</li></a>
+
+        </ul>
+      </nav>
+    </div>
+    </>
+  )
+}
+
+export default App
