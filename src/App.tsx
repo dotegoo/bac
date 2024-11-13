92038555fca238d3208ad3c63a2ac290d55f4f2c
Teodor Dobromir <teodor@Teodors-MacBook-Air.local>
2024-11-13 21:13:56 +0200

    13nov24' fixed nav + structure with div for each page

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,36 @@ function App() {
 
   return (
     <>
-    <div>
-      <h1>Bacu'</h1>
       <nav>
         <ul>
-          <a href=""><li>Romana</li></a>
-          <a href=""><li>Matematica</li></a>
-          <a href=""><li>Biologie</li></a>
-          <a href=""><li>Chimie</li></a>
-          <a href=""><li>Fizica</li></a>
-          <a href=""><li>Informatica</li></a>
-
+          <a href="#Romana"><li>Romana</li></a>
+          <a href="#Matematica"><li>Matematica</li></a>
+          <a href="#Biologie"><li>Biologie</li></a>
+          <a href="#Chimie"><li>Chimie</li></a>
+          <a href="#Fizica"><li>Fizica</li></a>
+          <a href="#Informatica"><li>Informatica</li></a>
         </ul>
       </nav>
+    <header>
+      <h1>Bacu'</h1>
+    </header>
+    <div id='Romana'>
+      <h1>Romana</h1>
+    </div>
+    <div id='Matematica'>
+      <h1>Matematica</h1>
+    </div>
+    <div id='Biologie'>
+      <h1>Biologie</h1>
+    </div>
+    <div id='Chimie'>
+      <h1>Chimie</h1>
+    </div>
+    <div id='Fizica'>
+      <h1>Fizica</h1>
+    </div>
+    <div id='Informatica'>
+      <h1>Informatica</h1>
     </div>
     </>
   )
